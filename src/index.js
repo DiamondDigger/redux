26a5b20291434c9a96e8819433ed2b555c0c0c4d
naglec13 <naglec13@gmail.com>
2020-09-06 01:53:21 +0300

    made connection between dom and JS logic - add/sub  button and counter of clicks via eventListeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css'
+
+const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
+
+let state = 0
+
+function render(){
+    counter.textContent = state.toString()
+}
+
+render()
+
+addBtn.addEventListener('click', () => {
+    state ++
+    render()
+})
+
+subBtn.addEventListener('click', () => {
+    state --
+    render()
+})
+
+
+
