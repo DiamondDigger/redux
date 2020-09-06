b98b38d792a12ee502cf326d1b3358f93b82d621
naglec13 <naglec13@gmail.com>
2020-09-06 16:05:08 +0300

    realized .dispatch, .subscribe and .getState in createStore.js

--- src/createStore.js
+++ src/createStore.js
@@ -1,8 +1,18 @@
-export function createStore(){
+export function createStore(rootReducer, initialState){
+    let state = rootReducer(initialState, {type: '__INIT__'})
+    const subscribers = []
 
     return {
-        dispatch(){},
-        subscribe(){},
-        getState(){}
+        // action = {type: 'INCREMENT'}
+        dispatch(state, action) {
+            state = rootReducer(state, action)
+            subscribers.forEach(sub => sub())
+        },
+        subscribe(callback) {
+            subscribers.push(callback)
+        },
+        getState() {
+            return state
+        }
     }
 }
