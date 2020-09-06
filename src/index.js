742eb9a165e7bb0668f8e0d2fcd0859cf598970b
naglec13 <naglec13@gmail.com>
2020-09-06 13:12:46 +0300

    added eventListener on async button and on theme button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './style.css'
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
 const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
+const themeBtn = document.getElementById('theme')
 
 let state = 0
 
@@ -22,5 +24,13 @@ subBtn.addEventListener('click', () => {
     render()
 })
 
+asyncBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        state ++
+        render()
+    }, 2000)
+})
 
-
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
